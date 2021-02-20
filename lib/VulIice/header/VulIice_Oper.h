368d89a728be0eae9300fb5d1b6e9f87fc2a67fb
PINABLINK - WEBER <pinablink@gmail.com>
2021-02-20 00:23:24 -0300

    Atualização de Backup de segurança

--- lib/VulIice/header/VulIice_Oper.h
+++ lib/VulIice/header/VulIice_Oper.h
@@ -1,24 +1,5 @@
 #ifndef VULIICE_OPER_H
 
-// CODIGOS DE OPERACAO
-#define OPER_LIMPEZA_CONTADORES_SERIAL           01H
-#define OPER_RECEBER_DADOS_SERIAL                02H
-#define OPER_RETORNAR_ERRO_ESTRUTURA_SERIAL      03H
-#define OPER_EXCLUIR_MENSAGEM_MEM_RAM            04H
-#define OPER_EXCLUIR_MENSAGEM_SERIAL_MEM_RAM     05H
-#define OPER_VALIDAR_CONTEUDO                    06H
-#define OPER_CRIAR_ERRO_ESTRUTURAL_MENSAGEM      07H
-#define OPER_CRIAR_ERRO_ESTRUTURAL_QTMK_MENSAGEM 08H
-
-// DEFINA UM REGISTRADOR QUE CONTERÁ UM CÓDIGO DE 16 BITS DA OPERAÇÃO QUE DEVERÁ
-// SER EXECUTADA. POR EXEMPLO, PARA UMA ROTINA CHAMAR LIMPEZA_CONTADORES_SERIAL
-// -----------------------------------------------------------------------------
-//       EXEMPLO:
-// -----------------------------------------------------------------------------
-// MOV.W #OPER_LIMPEZA_CONTADORES_SERIAL,VULIICE_OPER 
-
-#define VULIICE_OPER R11
-
 
 // É NECESSÁRIO 3 CONTADORES DE 16 BITS PARA REALIZAÇÃO DO PROCESSAMENTO
 // DO CONTEÚDO. DEFINA 3 REGISTRADORES PARA ESSA FINALIDADE
@@ -32,20 +13,25 @@
 
 #define  SUPORTE_CONTADOR_SERIAL_1 R8
 #define  SUPORTE_CONTADOR_SERIAL_2 R9
-#define  SUPORTE_CONTADOR_SERIAL_3 R10 
+#define  JSON_STRUCT_STATUS        R10 
+#define  CARACTER                  R11
 
 // MEMORIA RAM
 // CONTROLADORES
 // -----------------------------------------------------------------------------
 // TAMANHO DO CONTEÚDO
-#define   IIBYTE_TAMANHO_DADOS                00A00H
-#define   IBYTE_ERRO_FORMATO_DADO_SERIAL      045H
-#define   IBYTE_ERRO_FORMATO_SERIAL_QT_MARK   046H
-#define   IBYTE_ERRO_ESTOURO_MEMORIA_SERIAL   09DH  
-#define   IBYTE_SUCESSO_DADO_SERIAL           04FH
-#define   IBYTE_RECEBENDO_DADOS               06FH
-#define   IBYTE_DADOS_RECEBIDOS               07FH  
-#define   IBYTE_PROCESSO_AGUARDANDO           08FH
+#define   IIBYTE_TAMANHO_DADOS                 00A00H
+#define   IBYTE_ERRO_FORMATO_DADO_SERIAL       045H
+#define   IBYTE_ERRO_FORMATO_SERIAL_QT_MARK    046H
+#define   IBYTE_ERRO_FORMATO_UNKNOW_JSON       047H
+#define   IBYTE_ERRO_COMMA_CLOSING_NOT_ALLOWED 048H
+#define   IBYTE_ERRO_IN_OBJ_DUP_CUP            049H
+#define   IBYTE_ERRO_QUOT_MARKS_UNK_KEY_CONTEN 04AH
+#define   IBYTE_ERRO_ESTOURO_MEMORIA_SERIAL    09DH  
+#define   IBYTE_SUCESSO_DADO_SERIAL            04FH
+#define   IBYTE_RECEBENDO_DADOS                06FH
+#define   IBYTE_DADOS_RECEBIDOS                07FH  
+#define   IBYTE_PROCESSO_AGUARDANDO            08FH
 // -----------------------------------------------------------------------------
 // LSB          [XX] - 
 // MSB          [XX] - 
@@ -53,10 +39,11 @@
 // STATUS -     [XX] -   ERRO;  SUCESSO; AGUARDANDO
 // -----------------------------------------------------------------------------
 #define  CONF_LEN_DADOS           0203H
-#define  CONF_STATUS_SERIAL_DATA  0204H
 #define  CONF_STATUS_PROCESS      0205H
-#define  LEN_I_MESSAGE            0206H
-#define  LEN_SERIAL_MESSAGE       0207H // TAMANHO DA MENSAGEM GERADA COM A INFORMAÇÃO RECEBIDA
-#define  CONF_IN_SERIAL_DATA      0209H
+#define  LEN_I_MESSAGE            0206H   // TAMANHO DA MENSAGEM QUE SERÁ RETORNADA NA SERIAL
+#define  LEN_SERIAL_MESSAGE       0207H  //  TAMANHO DA MENSAGEM GERADA COM A INFORMAÇÃO RECEBIDA
+#define  LEN_KEY_CONTENT          0208H //   TAMANHO DO CONTEUDO DA CHAVE 
+#define  LEN_VALUE_CONTENT        0209H //   TAMANHO DO CONTEUDO DO VALOR
+#define  CONF_IN_SERIAL_DATA      020AH
 
 #endif
